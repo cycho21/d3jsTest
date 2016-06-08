741042bc1457c1f0e6118c0f99cb4b73736d3f46
chan8 <lovebube88@gmail.com>
2016-06-08 13:08:39 -0400

    add d3.min.js and update visualization

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ app.use(bodyparser.json());
 app.use('/htmls', express.static(__dirname + '/htmls'));
 
 app.get('/', function (req, res) {
-	res.redirect('/htmls/index.html');
+	res.redirect('/htmls/index2.html');
+});
+
+app.route('/getJSON')
+	.get(function(req, res) {
+	var tempJson = fs.readFileSync(__dirname + '/htmls/test.output');
+	res.json(JSON.parse(tempJson));
 });
 
 app.listen(3030, function () {
@@ -22,7 +28,7 @@ app.listen(3030, function () {
 });
 
 var rd = readline.createInterface({
-	input: fs.createReadStream('/root/logs/testLog.log.2016-06-04'),
+	input: fs.createReadStream('/root/logs/testLog.log.2016-06-06'),
 	output: process.stdout,
 	terminal: false
 });
@@ -46,5 +52,81 @@ rd.on('close', function() {
 	
 }
 	fs.writeFile(__dirname + "/htmls/test.output", JSON.stringify(resultArray));
-	console.log(resultArray);
 });
+
+
+
+function makeJSON(tempJson) {
+	var tempMap = {};
+	var tempArray = [];
+	tempMap = { "header": {
+		"title": {
+			"text": "Lots of Programming Languages",
+			"fontSize": 24,
+			"font": "open sans"
+		},
+		"subtitle": {
+			"text": "A full pie chart to show off label collision detection and resolution.",
+			"color": "#999999",
+			"fontSize": 12,
+			"font": "open sans"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 10,
+		"font": "open sans",
+		"location": "bottom-left"
+	},
+	"size": {
+		"canvasWidth": 590,
+		"pieOuterRadius": "90%"
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 32
+		},
+		"inner": {
+			"hideWhenLessThanPercentage": 3
+		},
+		"mainLabel": {
+			"fontSize": 11
+		},
+		"percentage": {
+			"color": "#ffffff",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#adadad",
+			"fontSize": 11
+		},
+		"lines": {
+			"enabled": true
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	},
+	"misc": {
+		"gradient": {
+			"enabled": true,
+			"percentage": 100
+		}
+	}
+};
+
+tempMap["data"] = {"sortOrder": "value-desc",
+		"content": JSON.parse(tempJson)
+		};
+	tempArray.push(tempMap);
+	console.log(tempMap);
+
+}
